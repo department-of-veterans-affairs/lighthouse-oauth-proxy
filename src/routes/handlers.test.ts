abdb8815f625ac639275f0efbdf77ee3fecd359c
Edward Paget <edpaget@users.noreply.github.com>
2018-12-09 23:43:38 -0600

    Add oauth proxy to repo (#15)
    * Implements an oauth proxy that makes our OAuth implementation FHIR-compliant
    
    * Adds a proxy-monitor service to monitor the saml-proxy and oauth-proxy containers on a single instance.

--- src/routes/handlers.test.ts
+++
@@ -1,8 +0,0 @@
-import "jest";
-import { getHashCode } from "./handlers"
-
-describe("getHashCode", () => {
-  test("It should get hash code for a string", () => {
-    expect(getHashCode("test-string")).toEqual(-1666277972);
-  });
-});
--- src/samlConstants.ts
+++
@@ -1,4 +0,0 @@
-export const BINDINGS = {
-  REDIRECT: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-  POST: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST'
-}

37d7bd6e0141a191efe8c3368c9b5b54ea060ada
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-09-29 12:25:11 -0600

    debug

--- src/scripts/check_for_git_secrets.sh
+++ src/scripts/check_for_git_secrets.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+INODE_NUM=`ls -ali / | sed '2!d' |awk {'print $1'}`
+if [ $INODE_NUM == '1' ];
+then
+  exit 0
+fi
+
 git secrets 2>&1 | grep "is not a git command" > /dev/null
 [ $? == 0 ] && echo -e "git secrets is not installed\nSee https://github.com/awslabs/git-secrets" && exit 1
 

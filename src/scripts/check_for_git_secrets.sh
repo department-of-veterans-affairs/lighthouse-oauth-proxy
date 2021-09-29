f5aac6c99a18c5254681d0f2221f21f0cf4c43dd
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-09-29 12:50:55 -0600

    debug

--- src/scripts/check_for_git_secrets.sh
+++ src/scripts/check_for_git_secrets.sh
@@ -4,6 +4,9 @@ if grep -sq 'docker\|lxc' /proc/1/cgroup; then
    exit 0
 fi
 
+if [ ! -z $CODE_BUILD_NUMBER]; then
+  exit 0
+fi
 git secrets 2>&1 | grep "is not a git command" > /dev/null
 [ $? == 0 ] && echo -e "git secrets is not installed\nSee https://github.com/awslabs/git-secrets" && exit 1
 

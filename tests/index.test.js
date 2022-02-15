dbfa81340b8c4af3fa15d1d6d9e0d2b6b17a6591
Larry Crochet <larry.crochet@libertyits.com>
2022-02-15 18:51:09 -0500

    test items

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,6 +7,14 @@ const dynnamoClient = {};
 jest.mock("axios");
 
 describe("proxymockin_request tests", () => {
+  const in_req = {
+    body: { client_id: "client1", token: "token1" },
+    headers: { host: "host1" },
+  };
+  // Don't alter req
+  jest.mock("../src/utils", () => {
+    jest.fn(() => Promise.resolve(in_req));
+  });
   it("proxymockin_request introspect", async () => {
     const mock_result = {
       active: true,
@@ -22,14 +30,6 @@ describe("proxymockin_request tests", () => {
       client_id: "client1",
       uid: "uid1",
     };
-    const in_req = {
-      body: { client_id: "client1", token: "token1" },
-      headers: { host: "host1" },
-    };
-    // Don't alter req
-    jest.mock("../src/utils", () => {
-      jest.fn(() => Promise.resolve(in_req));
-    });
 
     const res = {
       _status: "500",
@@ -65,6 +65,51 @@ describe("proxymockin_request tests", () => {
       promise2check(res);
     };
     axios.mockResolvedValueOnce(axiosPostPayload);
+    let config = createFakeConfig();
+    proxyRequest(
+      in_req,
+      res,
+      ISSUER_METADATA,
+      "introspection_endpoint",
+      "POST",
+      config,
+      dynnamoClient
+    );
+  });
+  it("proxymockin_request bad", async () => {
+    const promise2check = (res) => {
+      return new Promise(
+        (resolve) => {
+          expect(res._status).toBe("404");
+          expect(res.body).toBe("Bad request");
+          resolve(true);
+        },
+        (reject) => {
+          reject("Should not be here");
+        }
+      );
+    };
+    const axiosErrorPayload = {
+      body: "Bad request",
+      headers: { "Content-type": "application/json" },
+      status: "404",
+      data: {},
+    };
+    const res = {
+      _status: "500",
+    };
+    res.set = (in_headers) => {
+      res._headers = in_headers;
+    };
+    res.status = (in_status) => {
+      res._status = in_status;
+    };
+    axiosErrorPayload.data.pipe = (targetResp) => {
+      targetResp.body = axiosErrorPayload.body;
+      promise2check(res);
+    };
+
+    axios.mockRejectedValueOnce({ response: axiosErrorPayload });
     let config = createFakeConfig();
     proxyRequest(
       in_req,

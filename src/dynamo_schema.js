41773a5595525561da8b866e9409c8bdcfd1c9d2
Larry Crochet <larry.crochet@libertyits.com>
2022-04-05 09:10:39 -0400

    v2_client_id not needed

--- src/dynamo_schema.js
+++ src/dynamo_schema.js
@@ -313,26 +313,4 @@ function createTestClientEntry() {
       console.log(data);
     }
   });
-
-  itemParams = {
-    TableName: "Clients",
-    Item: {
-      client_id: { S: "testclient2" },
-      redirect_uris: {
-        SS: ["http://localhost:8080/auth/cb", "http://localhost:18080/auth/cb"],
-      },
-      system: { S: "oauthi" },
-    },
-  };
-  dynamo.putItem(itemParams, (err, data) => {
-    if (err) {
-      console.error(
-        "Unable to create test client. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-    } else {
-      console.log("Created test client.");
-      console.log(data);
-    }
-  });
 }
